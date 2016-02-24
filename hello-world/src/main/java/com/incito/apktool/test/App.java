d288e63878236b4b440b0bbec68bafcfff996024
caicai <837522223@qq.com>
2016-02-24 16:14:42 +0800

    士大夫

--- hello-world/src/main/java/com/incito/apktool/test/App.java
+++ hello-world/src/main/java/com/incito/apktool/test/App.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class App {
 
-	private int id;111111222222222222222222222222233
+	private int id;111111222223222222222222222222233
 	private String name;
 	private int code;
 	private String version;
